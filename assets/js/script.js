9c86043cb4623e1cc5ce7cf1b660b21462315b6e
BRosencrans <brandon.rosencrans@gmail.com>
2023-01-12 14:28:11 -0800

    added background

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,17 @@
 
-  document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('select');
-    var instances = M.FormSelect.init(elems, options);
-  });
+
+
+
+
+
+
+podURL = "https://api.nasa.gov/planetary/apod?api_key=NhPFpaLICBNtgWJTI0edILu5U5CcPKT8T4Fd2w2l" 
+
+window.addEventListener("load", function (){
+    fetch(
+      podURL = "https://api.nasa.gov/planetary/apod?api_key=NhPFpaLICBNtgWJTI0edILu5U5CcPKT8T4Fd2w2l" )
+      .then((response) => response.json())
+      .then((data) => {
+        document.body.style.backgroundImage = `url("${data.url}")`;
+        })
+})
