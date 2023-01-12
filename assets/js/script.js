b8ab9d979b6a695d517283fa8fdf5f2d52048637
Binh Ngo <bnngo97@gmail.com>
2023-01-12 14:18:21 -0800

    created url function with event listener

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,22 @@
 var userInput = document.getElementById("userInput");
+var searchButton = document.getElementById("searchButton")
 
-userInput.addEventListener("submit, ");
-
-
-
-document.addEventListener("DOMContentLoaded", function () {
-  var elems = document.querySelectorAll("select");
-  var instances = M.FormSelect.init(elems, options);
-});
 
 //fetch for planet and Stars
 // https://api-ninjas.com/api/planets for document regarding fetching data
-var planetUrl= "https://api.api-ninjas.com/v1/planets?name=Neptune";
+var planetUrl= "https://api.api-ninjas.com/v1/planets?name=" + userInput.value;
 //https://api-ninjas.com/api/stars for document regarding fetching data
-var starUrl = "https://api.api-ninjas.com/v1/stars?name=vega";
+var starUrl = "https://api.api-ninjas.com/v1/stars?name=" + userInput.value;
 
-var requestUrl = 'https://images-api.nasa.gov/search?q=neptune&media_type=image';
+var requestUrl = 'https://images-api.nasa.gov/search?q=' + userInput.value + '&media_type=image';
+
+searchButton.addEventListener("click", createUrl);
+
+function createUrl () {
+  planetUrl= "https://api.api-ninjas.com/v1/planets?name=" + userInput.value;
+  starUrl = "https://api.api-ninjas.com/v1/stars?name=" + userInput.value;
+  requestUrl = 'https://images-api.nasa.gov/search?q=' + userInput.value + '&media_type=image';
+}
 
 
 
@@ -28,14 +29,14 @@ function fetchStar(){
       'Content-Type': "application/json",
     },
   })
-    .then(function (response) {
-      return response.json();
+  .then(function (response) {
+    return response.json();
     })
     .then(function (data) {
       console.log(data);
     });
   }
-
+  
 function fetchPlanet(){
   fetch(planetUrl, {
     method: 'GET',
@@ -44,27 +45,32 @@ function fetchPlanet(){
       'Content-Type': "application/json",
     },
   })
-    .then(function (response) {
-      return response.json();
-    })
-
-    .then(function (data) {
-
-      console.log(data);
-    });
-  }
+  .then(function (response) {
+    return response.json();
+  })
+  
+  .then(function (data) {
+    
+    console.log(data);
+  });
+}
 
 function fetchNasa(){
-    fetch(requestUrl)
-      .then(function (response) {
+  fetch(requestUrl)
+  .then(function (response) {
          return response.json();
-     })
-      .then(function (data) {
-         console.log(data);
-  
-        
-     });
-  }
-
-  fetchStar();
-  fetchPlanet();
+        })
+        .then(function (data) {
+          console.log(data);
+          
+          
+        });
+      }
+      
+      fetchStar();
+      fetchPlanet();
+      
+      document.addEventListener("DOMContentLoaded", function () {
+        var elems = document.querySelectorAll("select");
+        var instances = M.FormSelect.init(elems, options);
+      });
